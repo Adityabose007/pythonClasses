a996f9c8effd36c6eb7fed6fd8a3600f1cc9fdd5
Aditya Bose <boseaditya680@gmail.com>
2025-06-26 22:48:16 +0530

    day_1

--- day_1.py
+++ day_1.py
@@ -14,4 +14,4 @@ print(calculator(10, 5, '+'))  # Output: 15
 print(calculator(10, 5, '-'))  # Output: 5
 print(calculator(10, 5, '*'))  # Output: 50
 print(calculator(10, 5, '/'))  # Output: 2.0
-
+print(calculator(10, 5, '%'))  # Output: Invalid operator
