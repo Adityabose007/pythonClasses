394c8a9662de8a18b8d88ada19f26430164b80a9
Aditya Bose <boseaditya680@gmail.com>
2025-06-26 22:21:52 +0530

    day_1

--- day_1.py
+++ day_1.py
@@ -15,20 +15,3 @@ print(calculator(10, 5, '-'))  # Output: 5
 print(calculator(10, 5, '*'))  # Output: 50
 print(calculator(10, 5, '/'))  # Output: 2.0
 
-
-
-
-# Variable assignment and comment
-name = "Aditya"
-age = 20
-rating = 4.5
-
-# Print the values
-print("Name:", name)
-print("Age:", age)
-print("Rating:", rating)
-
-# Check data types
-print(type(name))  # str
-print(type(age))   # int
-print(type(rating))  # float
