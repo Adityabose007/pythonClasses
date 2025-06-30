7bb623c66bad00844f24605ac6f4506b688cb94f
Aditya Bose <boseaditya680@gmail.com>
2025-06-30 14:05:26 +0530

    day_3.py

---
+++ day_3.py
@@ -0,0 +1,32 @@
+#for(int i =0; i<=10; i++)
+#for i in range(start, stop, step):
+    
+'''for i in range(5):
+    print(i)'''
+
+'''num = 5
+a = 1
+
+if (num<0):
+        print("Factorial is not defined for negative numbers")
+elif (num == 0 or num == 1):
+        print("Factorial is 1")
+for i in range(2, num + 1):
+        a *= i
+print("Factorial is", a)'''
+
+
+
+a = 0 
+b = 1
+n = 10
+
+print("fibonacci Series:")
+for i in range(n):
+    
+    print(a, end=' ')
+    a,b = b, a+b
+    
+
+
+           
