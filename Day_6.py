2d5f5a27b561219e4776e8f66a9443d7fcf8df4c
Aditya Bose <boseaditya680@gmail.com>
2025-07-09 11:36:09 +0530

    day_6

--- Day_6.py
+++ Day_6.py
@@ -181,7 +181,7 @@ text = input("Enter a string: ")
 result = ""
 for char in text:
     if char not in result:
-        result += char
+        result =result + char
 print("String after removing duplicates:", result)
 
 
