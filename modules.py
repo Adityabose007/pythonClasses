f8ebad66a7e11ae21d054f1c548253f8d58784ff
Aditya Bose <boseaditya680@gmail.com>
2025-07-16 14:31:31 +0530

    day_10

---
+++ modules.py
@@ -0,0 +1,18 @@
+import math as m
+import random as r
+import datetime as dt
+#print(math.sqrt(25))
+'''print(m.sqrt(36))
+print(m.factorial(6))
+print(m.pi)
+print(m.floor(3.44))
+
+
+print(r.radiant(1,6))
+print(r.choice(['a','b','c','d','e','f']))
+li - [1,2,3,4,5,6,7]
+r.shuffle(li)
+print(li)'''
+
+
+now 
